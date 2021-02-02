08d97789ec6eef1c6aa2ad727c7efa3076ef6d28
Sam Larkin <selarkin96@gmail.com>
2021-02-02 14:31:57 +0000

    get rid of trash

--- morsetools/test_morsetools.py
+++ morsetools/test_morsetools.py
@@ -1,15 +1,6 @@
-import ../morsetools
 import os
-
-wd = os.getcwd()
-dictionary_path = os.path.join(wd,'dictionaries','morse_dictionary.json')
-dictionary = morsetools.Dictionary(dictionary_path)
-
-test_string = 'the quick brown fox jumps over the lazy dog'
-test_text = morsetools.message.Text(test_string, dictionary)
-textpath = os.path.join(wd, 'outputs', 'test_text.txt')
-test_text.write(textpath)
-test_morse = test_text.encode()
-outpath = os.path.join(wd, 'outputs', 'test_morse.txt')
-test_morse.write(outpath)
-test_bounceback = test_morse.decode()
+import unittest
+from .dictionary import Dictionary
+from .message import Message
+from .message import Text
+from .message import Morse
