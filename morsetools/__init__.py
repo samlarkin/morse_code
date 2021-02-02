c6dd128429d4ec39ccf71b5b9431592ca1bfa69d
Sam Larkin <selarkin96@gmail.com>
2021-02-02 14:23:37 +0000

    remove imports from __init__.py

--- morsetools/__init__.py
+++ morsetools/__init__.py
@@ -1,10 +1,8 @@
-"""
-morse.py
+""" morsetools
+
 author: Sam Larkin
 url: https://github.com/samlarkin/morse_code
 
-Module written as a learning exercise
 Encodes text to Morse code and vice versa
+
 """
-from .dictionary import Dictionary
-from .message import Message, Text, Morse
---
+++ morsetools/__main__.py
@@ -0,0 +1,2 @@
+from .dictionary import Dictionary
+from .message import Message, Text, Morse
